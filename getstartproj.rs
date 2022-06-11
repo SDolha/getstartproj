ca24671d5a47c8e07a041324571e1263c2cd1a83
Sorin Dolha <sdolha@hotmail.com>
2022-06-11 20:55:50 +0300

    Get projects in solution

--- getstartproj.rs
+++ getstartproj.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::process;
 use std::path::Path;
 use std::ffi::OsStr;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -51,6 +52,32 @@ fn process_sln_file(sln_path: &Path, original_path: &Path) {
     get_start_projects(sln_path)
 }
 
-fn get_start_projects(_sln_path: &Path) {
-    println!("\t(root projects will be presented here)");
+fn get_start_projects(sln_path: &Path) {
+    let project_files = get_project_paths(sln_path);
+    println!("\t(temporary count of projects: {})", project_files.len()); // will be replaced later
+}
+
+fn get_project_paths(sln_path: &Path) -> Vec<String> {
+    let mut project_paths = Vec::<String>::new();
+    if let Ok(contents) = fs::read_to_string(sln_path) {
+        for line in contents.lines() {
+            let project_indicator = "Project";
+            let project_indicator_len = project_indicator.len();
+            let csproj_indicator = ".csproj";
+            let vbproj_indicator = ".vbproj";
+            let start_separator = ", \"";
+            let end_separator = "\", ";
+            if line.starts_with(project_indicator) && (line.contains(csproj_indicator) || line.contains(vbproj_indicator)) {
+                if let Some(start_index) = line[project_indicator_len..].find(start_separator) {
+                    let start_index = project_indicator_len + start_index + start_separator.len();
+                    if let Some(end_index) = line[start_index..].find(end_separator) {
+                        let end_index = start_index + end_index;
+                        let project_path = &line[start_index..end_index];
+                        project_paths.push(project_path.to_string());
+                    }
+                }
+            }
+        } 
+    }
+    return project_paths;
 }
